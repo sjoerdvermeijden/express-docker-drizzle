0dfc489804cbfaed0804e2d6755a20fd1136908e
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-04-29 11:14:40 +0200

    Adds a new name to the controller

--- controllers/userControllers.mjs
+++ controllers/userControllers.mjs
@@ -15,6 +15,10 @@ export const getUsers = async (req, res) => {
         {
             id: 1,
             name: 'Tom'
+        },
+        {
+            id: 2,
+            name: 'Tim'
         }
     ])
 };

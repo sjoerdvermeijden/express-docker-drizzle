8b1ee2cc76e1d8bc541281ad2c72ea40c33771d9
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-04-29 11:07:05 +0200

    Adds drizzle stuff to userController

--- controllers/userControllers.mjs
+++ controllers/userControllers.mjs
@@ -1,3 +1,11 @@
+// import { users } from '../schema.ts'
+// import { db } from '../db/db.mjs'
+
+// export const getUsers = async (req, res) => {
+//     const result = await db.select().from(users);
+//     res.json(result)
+// };
+
 export const getUsers = async (req, res) => {
     res.json([
         {
